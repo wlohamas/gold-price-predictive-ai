5f877c62307aaa0dd2642d1bc9a5932a5a4928d0
Wittawat Lohamas <wlohamas@gmail.com>
2026-02-02 15:29:31 +0700

    Prepare for Render.com deployment

--- app.py
+++ app.py
@@ -108,4 +108,6 @@ if __name__ == '__main__':
     t.start()
     
     # Start Flask server
-    app.run(debug=True, port=5001, use_reloader=False) # use_reloader=False to avoid duplicate threads
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)

888ca933e89ee4b306987b7484efae1377b3dd75
Wittawat Lohamas <wlohamas@gmail.com>
2026-02-02 18:58:46 +0700

    Enable Flask debug mode and reloader for local development

--- app.py
+++ app.py
@@ -121,4 +121,4 @@ if __name__ == '__main__':
     # Start Flask server
     import os
     port = int(os.environ.get("PORT", 5001))
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
